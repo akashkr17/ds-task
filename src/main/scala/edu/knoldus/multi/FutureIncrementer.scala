b1fbbe5d3301d459ec03c37739d20249c781d3c2
akashkr17 <akash.kumar@knoldus.com>
2022-06-09 15:55:48 +0530

    add the future counter

---
+++ src/main/scala/edu/knoldus/multi/FutureIncrementer.scala
@@ -0,0 +1,18 @@
+package edu.knoldus.multi
+
+import java.util.concurrent.Executors
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.concurrent.duration.DurationInt
+
+object FutureIncrementer extends App {
+var count = 0
+	implicit val ec: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2000))
+	val tasks: Seq[Future[Unit]] = for (i <- 1 to 2000) yield Future {
+		// do something more fancy here
+		synchronized {
+			count += 1
+			println(s"Thread ${Thread.currentThread().getName} is running. Counter = $count")
+		}
+	}
+	println(s"Counter = $count")
+}
