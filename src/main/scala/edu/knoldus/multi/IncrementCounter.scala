0316f0f73ff1cee7228acf2768b372fc366c8740
akashkr17 <akash.kumar@knoldus.com>
2022-06-09 13:40:44 +0530

    fix counter

--- src/main/scala/edu/knoldus/multi/IncrementCounter.scala
+++ src/main/scala/edu/knoldus/multi/IncrementCounter.scala
@@ -2,26 +2,25 @@ package edu.knoldus.multi
 
 import java.util.concurrent.atomic.AtomicInteger
 
-class IncrementCounter() extends Runnable {
-
-	import IncrementCounter._
-
-	override def run(): Unit = {
-		count.incrementAndGet()
-		println(s"Thread ${Thread.currentThread().getName} is running. Counter = $count")
-	}
-}
 
 // Creating object
-object IncrementCounter {
+object IncrementCounter extends App {
 	// Main method
 	private var count = new AtomicInteger(0)
 
-	def main(args: Array[String]) {
-		for (x <- 1 to 2000) {
-			val th = new Thread(new IncrementCounter())
-			th.setName(x.toString)
-			th.start()
+
+	for (x <- 1 to 2000) {
+		val th = new Thread(new Increment())
+		th.setName(x.toString)
+		th.start()
+	}
+
+	class Increment extends Runnable {
+
+
+		override def run(): Unit = {
+			count.incrementAndGet()
+			println(s"Thread ${Thread.currentThread().getName} is running. Counter = $count")
 		}
 	}
 }
