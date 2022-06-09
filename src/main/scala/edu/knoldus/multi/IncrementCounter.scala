6779625b12c2e5cf7b81a5e3334677321ed26368
akashkr17 <akash.kumar@knoldus.com>
2022-06-09 15:37:00 +0530

    add the join in counter

--- src/main/scala/edu/knoldus/multi/IncrementCounter.scala
+++ src/main/scala/edu/knoldus/multi/IncrementCounter.scala
@@ -5,20 +5,25 @@ import java.util.concurrent.atomic.AtomicInteger
 
 object IncrementCounter extends App {
 
-	private var count = new AtomicInteger(0)
+	private var count = 0
 
 	for (x <- 1 to 2000) {
 		val th = new Thread(new Increment())
 		th.setName(x.toString)
 		th.start()
+		th.join()
 	}
 
 	class Increment extends Runnable {
 
 		override def run(): Unit = {
-			count.incrementAndGet()
-			println(s"Thread ${Thread.currentThread().getName} is running. Counter = $count")
+			synchronized {
+				count += 1
+				println(s"Thread ${Thread.currentThread().getName} is running. Counter = $count")
+			}
+
 		}
 	}
 }
+
 
